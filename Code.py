bf6d9c9c8ab848189c734b682225a1760c673059
Lisa Navarro <56326171+LisaNavarro@users.noreply.github.com>
2019-11-02 13:54:07 -0400

    Update Code.py

--- Code.py
+++ Code.py
@@ -5,6 +5,16 @@ import os
 import csv
 
 #The Variables
+total_months=0
+total_profit/losses=0
+average_change_Profit/losses=0
+
+#Print the following chart
+
+print("Financial Analysis")
+print("----------------------------------------------"
+print("Total Months: ")
+
 
 
 
